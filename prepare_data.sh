7093f20a821dbc3bbb10bcbddbb487de6766d2c5
jeff-rp <57658601+jeff-rp@users.noreply.github.com>
2021-06-22 12:45:33 +0800

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,10 +1,13 @@
 cd coco
 wget http://images.cocodataset.org/zips/train2017.zip
-wget http://images.cocodataset.org/zips/val2017.zip
-wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 unzip -q train2017.zip
+rm train2017.zip
+wget http://images.cocodataset.org/zips/val2017.zip
 unzip -q val2017.zip
+rm val2017.zip
+wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 unzip -q annotations_trainval2017.zip
+rm annotations_trainval2017.zip
 cd ..
 cd data
 wget https://github.com/RedPillLab/lightweight-human-pose-estimation.pytorch/releases/download/v0.1/prepared_train_annotation_1.pkl
